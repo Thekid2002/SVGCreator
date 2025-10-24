ed0cb957fe8f71f357ffe3788d71ecc81308c3bc
Thomas Andersen <55490372+Thekid2002@users.noreply.github.com>
2025-10-24 15:34:48 +0200

    SVG Fix

---
+++ SVGCreatorTest/SvgDocumentTest.cs
@@ -0,0 +1,20 @@
+﻿using SVGCreator;
+
+namespace SVGCreatorTest;
+
+public class SvgDocumentTest
+{
+    [Fact]
+    public void Test1()
+    {
+        var document = new SvgDocument().SetWidth(100)
+            .SetHeight(100)
+            .AddCircle(50, 50, 40, System.Drawing.Color.Red, System.Drawing.Color.Black, 2)
+            .ToString();
+        
+        var expected = @"<svg width=""100"" height=""100"" xmlns=""http://www.w3.org/2000/svg"">
+            <circle cx=""50"" cy=""50"" r=""40"" stroke=""Black"" stroke-width=""2"" fill=""Red""/>
+        </svg>";
+        Assert.Equal(expected.Replace(" ", "").Replace("\n", ""), document.Replace(" ", "").Replace("\n", ""));
+    }
+}
