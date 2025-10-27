1deeb25f727e4252e7a62d2e57bc4a0f10d32fcd
Thomas Andersen <55490372+Thekid2002@users.noreply.github.com>
2025-10-27 08:36:41 +0100

    Update ISvgDocument.cs

--- SVGCreator/ISvgDocument.cs
+++ SVGCreator/ISvgDocument.cs
@@ -9,7 +9,7 @@ public interface ISvgDocument
     SvgDocument AddCircle((int x, int y) center, int radius, Color? fillColor, Color? strokeColor, int? strokeWidth, string? additionalAttributesString = null);
     SvgDocument AddLine((int x, int y) start, (int x, int y) end, Color strokeColor, int strokeWidth, string? additionalAttributesString = null);
     SvgDocument AddCurvedLine(List<(int, int)> points, Color strokeColor, int strokeWidth, string? additionalAttributesString = null);
-    SvgDocument AddText(string content, (int x, int y) centrum, int fontSize, string? fontFamily, int? rotation, Color? fillColor, Color? strokeColor, int? strokeWidth, string? additionalAttributesString = null);
+    SvgDocument AddText(string content, (int x, int y) center, int fontSize, string? fontFamily, int? rotation, Color? fillColor, Color? strokeColor, int? strokeWidth, string? additionalAttributesString = null);
     SvgDocument AddSquare((int x, int y) center, int sideLength, Color? fillColor, Color? strokeColor, int? strokeWidth, string? additionalAttributesString = null);
     SvgDocument AddRectangle((int x, int y) center, int width, int height, Color? fillColor, Color? strokeColor, int? strokeWidth, string? additionalAttributesString = null);
     string ToString();
